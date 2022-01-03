9e53b4ff9126e0b2b5a5096fd0f3af4f6325c96d
Jaloldin <jaloliddinfas@gmail.com>
2022-01-03 20:54:56 +0500

    ad

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_click_me.setOnClickListener(){
+            startSec()
+        }
+    }
+    fun startSec(){
+        val intent=Intent(this,SecondAct::class.java)
+        startActivity(intent)
     }
 }
